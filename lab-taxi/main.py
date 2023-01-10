794512ba13f73527604e31bf588074d16808d315
JiaRuiming-1 <115370574+JiaRuiming-1@users.noreply.github.com>
2023-01-10 16:29:08 +0800

    Update main.py

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -4,5 +4,7 @@ import gym
 import numpy as np
 
 env = gym.make('Taxi-v2')
-agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+# agent = Agent()
+# avg_rewards, best_avg_reward = interact(env, agent)
+
+
