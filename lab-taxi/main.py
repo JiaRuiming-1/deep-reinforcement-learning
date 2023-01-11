c73be871f5f306cd44278563c35f82d31eb58211
JiaRuiming-1 <115370574+JiaRuiming-1@users.noreply.github.com>
2023-01-11 16:18:36 +0800

    Update main.py

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -12,27 +12,42 @@ passenger_loc_set = {0:'Red', 1:'Green', 2:'Yellow', 3:'Blue', 4:'in taxi'}
 des_set = {0:'Red', 1:'Green', 2:'Yellow', 3:'Blue'}
 loc_coordinate_set = {0:[0,0], 1:[0,4], 2:[4,0], 3:[4,3], 4:[-1,-1]}
 
-
-state = env.reset()
-# state[0] = [(taxi_row*5 + taxi_col)*5 + pass_loc] * 4 + dest_idx
-print(state)
-print(f'current location:{list(env.decode(state[0]))[:2]}')
-print(f'passenger {loc_coordinate_set[list(env.decode(state[0]))[2]]}->{loc_coordinate_set[list(env.decode(state[0]))[3]]}')
-
-
-# next_state, reward, done, _ 
-action = env.action_space.sample()
-next_state, reward, done, _, info = env.step(4)
-print((next_state, reward, done, _, info))
-print(action, action_name_set[action])
-print(f'current location:{list(env.decode(next_state))[:2]}')
-print(f'passenger {loc_coordinate_set[list(env.decode(next_state))[2]]}->{loc_coordinate_set[list(env.decode(next_state))[3]]}')
-
+'''
+train agent
+'''
 agent = Agent()
 avg_rewards, best_avg_reward = interact(env, agent, num_episodes=20000)
+
+'''
+plot average reward
+'''
 import matplotlib.pyplot as plt
 avg_scores2 = [x for ii,x in enumerate(avg_rewards) if ii>9999]
 plt.plot(np.linspace(0,20000,len(avg_rewards),endpoint=False), np.asarray(avg_rewards))
 plt.show()
 plt.plot(np.linspace(10000,20000,len(avg_scores2),endpoint=False), np.asarray(avg_scores2))
 plt.show()
+
+'''
+chek optimal perfomance of agent 
+'''
+import time
+env = gym.make("Taxi-v3", render_mode="human")
+state = env.reset()[0]
+time.sleep(1)
+score = 0
+qtable = agent.Q
+while True:
+    env.render()
+    time.sleep(0.5)
+    action = np.argmax(qtable[state])
+    next_state, reward, done, _, info = env.step(action)
+    score += reward
+    
+    state = next_state
+    if done:
+        print(score)
+        break
+env.close()
+
+
